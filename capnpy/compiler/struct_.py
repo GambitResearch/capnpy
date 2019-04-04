17dc3c90f7e5a8d0a4e796c768dece01b720afe3
Antonio Cuni <anto.cuni@gmail.com>
2019-04-04 18:15:03 +0200

    emit __capnpy_id__ also for modules and types

--- capnpy/compiler/struct_.py
+++ capnpy/compiler/struct_.py
@@ -48,6 +48,7 @@ class Node__Struct:
             child.emit_definition(m)
         #
         ns = m.code.new_scope()
+        ns.id = self.id
         ns.name = self.compile_name(m)
         ns.dotname = self.runtime_name(m)
         ns.data_size = self.struct.dataWordCount
@@ -61,6 +62,7 @@ class Node__Struct:
         #
         with ns.block("{cdef class} {name}(_Struct):"):
             ns.ww("""
+                __capnpy_id__ = {id:#x}
                 __static_data_size__ = {data_size}
                 __static_ptrs_size__ = {ptrs_size}
 
